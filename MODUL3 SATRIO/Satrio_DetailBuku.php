f7737d40e4a18602441b4a3df003277e78b6cd90
satrio wicaksono <iam.satriorahman28@gmail.com>
2021-11-22 14:03:02 +0700

    Fixed Bug

--- MODUL3 SATRIO/Satrio_DetailBuku.php
+++ MODUL3 SATRIO/Satrio_DetailBuku.php
@@ -110,7 +110,7 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <form action="Satrio_DetailBuku.php" method="post" enctype="multipart/form-data">
+            <form action="Satrio_Update.php" method="post" enctype="multipart/form-data">
               <input type="hidden" name="id" value=<?=$data['id_buku']?>>
 
                 <div class="form-group mt-3">
